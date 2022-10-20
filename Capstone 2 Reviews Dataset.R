7425be38fa59edae96f2e8ad09f37d585a16f1c8
UttamSaket <116080504+UttamSaket@users.noreply.github.com>
2022-10-20 23:01:29 +0530

    Update Capstone 2 Reviews Dataset.R

--- Capstone 2 Reviews Dataset.R
+++ Capstone 2 Reviews Dataset.R
@@ -1,4 +1,6 @@
 
+![test image size]<img src="https://fullpath/assets/yourgif.gif" width="400" height="790">
+
 
 # Uttam Saket
 # Capstone project no. 2 - Sentiment Analysis on "Reviews.csv dataset"
